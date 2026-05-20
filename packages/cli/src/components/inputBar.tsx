3bc2569a25045c7e4316dd4e881e571d59f6513d
Shivpratap <shivpratap05oct2007@gmail.com>
2026-05-20 13:37:18 +0530

    command menu almost done

--- packages/cli/src/components/inputBar.tsx
+++ packages/cli/src/components/inputBar.tsx
@@ -1,6 +1,7 @@
 import type { KeyBinding } from "@opentui/core";
 import { EmptyBorder } from "./boarder";
 import { StatusBar } from "./statusBar";
+import { CommandMenu } from "../command-menu";
 
 type Props = {
   onSubmit: (text: string) => void;
@@ -36,6 +37,18 @@ export function InputBar({ onSubmit, disabled = false }: Props) {
           width={"100%"}
           gap={1}
         >
+          {true && (
+            <box
+              position="absolute"
+              bottom={"100%"}
+              left={0}
+              width={"100%"}
+              backgroundColor={"#1A1A24"}
+              zIndex={10}
+            >
+              <CommandMenu query=""/>
+            </box>
+          )}
           <textarea
             focused={!disabled}
             placeholder={'Ask anything... "Fix a bug in database'}
